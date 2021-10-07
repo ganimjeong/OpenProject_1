f2c8d46aba3acf815fdd42fd8137b693067fcc20
ganimjeong <ganimjeong@gmail.com>
2021-10-07 16:07:42 +0900

    init project

---
+++ Develop/workspace/OpenProject1/src/edu/Main.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package edu;
+
+/**
+ * @author jeong-ganim
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//변수선언
+		int firstno, secondno;
+		int sum;
+		
+		//두수 입력
+		firstno= 10;
+		secondno = 20;
+		
+		//합계내
+		sum = firstno + secondno;
+		
+		//합계출력
+		System.out.println("합계: "+ sum);
+
+	}
+
+}
